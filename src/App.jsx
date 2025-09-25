a60077756abc77adbadb6ae91a3fda472528a7bb
sehoon0529 <kim44415953@gmail.com>
2025-09-25 17:42:49 +0900

    💄Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import TickerCard from './TicketCard'
 
+
 const stocks = [
   { ticker: 'NVDA', name: '엔비디아', currentPrice: 178.43, previousClose: 181.40 },
   { ticker: 'TSLA', name: '테슬라', currentPrice: 425.85, previousClose: 439.95 },
@@ -10,9 +11,9 @@ const stocks = [
 
 function App() {
   return (
-    <div>
-      <h1>오늘의 주식 시세</h1>
-      <div>
+    <div className='bg-blue-100 min-h-screen p-8 flex flex-col items-center'>
+      <h1 className="text-4xl font-bold text-gray-800 mb-10">오늘의 주식 시세</h1>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full max-w-7xl">
         {
           stocks.map(stock => (
             <TickerCard
